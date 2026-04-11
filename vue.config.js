38663712dc2ca24a7b668eefb546c529d9800f0e
fireworks99 <2639237361@qq.com>
2026-04-11 12:35:01 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  output: {
-    publicPath: '/learncesium/'
-  }
+  transpileDependencies: true
 })
