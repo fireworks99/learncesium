08c758261cc2654d60724df967e13de853c69905
fireworks99 <46671672+fireworks99@users.noreply.github.com>
2026-06-01 06:59:50 +0000

    Deploying to gh-pages from @ fireworks99/learncesium@dfafc347bcbeab48f51f36a746a9580bcee13187 🚀

---
+++ config.js
@@ -0,0 +1,11 @@
+window.APP_CONFIG = {
+  online_map: true,
+  urltemplate_api: "http://localhost:8093/",
+  urltemplate_epsg: "3857",
+  first: 'z',
+  second: 'x',
+  third: 'reverseY',
+  format: 'jpg',
+  max_zoom: 10,
+  terrain_api: "http://localhost:8094/"
+}
